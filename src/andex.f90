68b02948a0bcf741587ef1d86477ecde8e4b8e13
Eric Nussbaumer <ebaumer@ucar.edu>
2014-01-13 09:48:40 -0700

    Add GNU GPL license to sift code. This involves adding a header to each file and a license file.

--- src/andex.f90
+++ src/andex.f90
@@ -1,5 +1,22 @@
-! ----------------------------------------------------------------
+!-----------------------------------------------------------------------------
+!    Copyright (c) 2013-2014 NDACC/IRWG
+!    This file is part of sfit.
 !
+!    sfit is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    any later version.
+!
+!    sfit is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with sfit.  If not, see <http://www.gnu.org/licenses/>
+!-----------------------------------------------------------------------------
+
+
       REAL (8) FUNCTION ANDEX (H,SH,GAMMA)
 !
 !     DOUBLE PRECISION VERSION OF ANDEX - NEEDED FOR IMPROVED GEOMETRY
