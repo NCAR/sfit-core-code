773be3327ba711e526e43f53ef82b9e0d058286e
Mathias Palm <mathias.palm@uni-bremen.de>
2016-03-31 19:51:09 +0200

    l to correct matrices whose lines are truncated due to sfit4

---
+++ tools/correct_matrix/correct_matrices.f90
@@ -0,0 +1,34 @@
+program convert
+
+  ! corrects matrices to have one row in the matrix in one row of the file
+  ! 
+  implicit none
+  character (len=10000) :: tmp, read_format, write_format
+  integer :: ind, nr_row, nr_column, itmp, ind2, itmp2
+  real(kind=8), allocatable, dimension(:) :: row 
+
+  
+!  call system ('mv g.out g.out.orig')
+  open(10, file = 'g.out.orig', status='old')
+  open(20, file = 'g.out.new', status='unknown')
+
+  read(10, *) tmp! skip first line
+  write(20,'(A)') trim(tmp)
+  read(10, *), nr_row, nr_column, itmp, itmp2
+  write(20, *) nr_row, nr_column, itmp, itmp2
+  read(10, '(A)') tmp! skip third line
+  write(20, *) trim(tmp)! skip third line
+
+  allocate(row(nr_column))
+  
+  write(write_format, '(a,i5,a)') '(', nr_column, 'ES26.18)'  
+  do ind = 1,nr_row
+     read(10,*) row
+     write(20,trim(write_format)) row
+  end do
+
+  deallocate(row)
+  close(10)
+  close(20)
+  
+end program convert
