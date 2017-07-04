8adb73b432f4784eca89c0ef71b3aaaa709940b7
Mathias <mathias@mini-mp.uni-bremen.de>
2017-07-04 14:49:05 +0200

    Started inclusion of MTCKD continuum

--- src/continuum.f90
+++ src/continuum.f90
@@ -8,12 +8,14 @@ module continuum
   integer, parameter :: cont_poly_max = 10
 
   integer :: ncont = 0, n_contabs
-  logical :: f_continuum = .false.
+  logical :: f_continuum = .false., f_mtckd = .false.
   integer :: abscont_type, abscont_order
   real(double), dimension(cont_poly_max) :: abscont_param, abscont_sparam
   real(double), dimension(:), allocatable :: cont_param
+  real(double), dimension(:,:,:), allocatable :: mtckd
   real(double) :: cont_z_abs, cont_alpha
-
+  
+  
 contains
 
   subroutine calc_continuum(param)
@@ -107,4 +109,91 @@ contains
        end select
     end if
   end subroutine calc_continuum
+
+  subroutine h2o_continuum()
+    ! calculates the continuum absorption for a give atmoshere
+    ! It is a wrapper for the MT-CKD continuum and sets up the variables for
+
+    implicit none
+    integer iband,k, kk
+    real (double) :: wtot, wa, wn2, vmrh2o,w_dry
+    real (double) :: V1ABS,V2ABS,DVABS,NPTABS,ABSRB,xlength
+    real (double) :: XID,SECANT,PAVE,TAVE,HMOLID,XALTZ
+    real (double) :: WK,PZL,PZU,TZL,TZU,WBROAD,DV,V1 ,V2 ,TBOUND
+    real (double) :: EMISIV,FSCDID,NMOL_C,LAYER ,YI1,YID,LSTWDF  
+    COMMON /ABSORB/ V1ABS,V2ABS,DVABS,NPTABS,ABSRB(n_absrb)
+    COMMON /FILHDR/ XID(10),SECANT,PAVE,TAVE,HMOLID(60),XALTZ(4),     &
+         &                WK(60),PZL,PZU,TZL,TZU,WBROAD,DV ,V1 ,V2 ,TBOUND, &
+         &                EMISIV,FSCDID(17),NMOL_C,LAYER ,YI1,YID(10),LSTWDF  
+
+    do iband = 1, nband
+       do k = 1, kmax-1
+          pave = p(k)
+          tave = t(k)
+          xlength = (z(k+1) - z(k))*10000.0d0 !thickness needed in cm
+
+          vmrh2o = xgas(1,k)
+          wtot = xgas(1,k)*ccc(iband,k)
+          do kk = 1, nret
+             if( trim(name(igas(kk))) .eq. trim('H2O')) then
+                vmrh2o = x(kk,k)
+                wtot = x(kk,k)*ccc(iband,k)
+             end if
+          end do
+          
+          w_dry = wtot * (1.-vmrh2o)
+          !ARGON
+          WA     = 0.009     * W_dry 
+          
+          !NITROGEN
+          wn2 = xgas(41,k) * ccc(iband,k)
+          do kk = 1, nret
+             if( trim(name(igas(kk))) .eq. trim('N2')) then
+                wn2 = x(kk,k)*ccc(iband,k)
+             end if
+          end do
+
+          ! CO2
+          wk(2) = xgas(2,k)*ccc(iband,k)
+          do kk = 1, nret
+             if( trim(name(igas(kk))) .eq. trim('CO2')) then
+                wk(2) = x(kk,k)*ccc(iband,k)
+             end if
+          end do
+
+          ! Ozone
+          wk(3) = xgas(3,k)*ccc(iband,k)
+          do kk = 1, nret
+             if( trim(name(igas(kk))) .eq. trim('O3')) then
+                wk(3) = x(kk,k)*ccc(iband,k)
+             end if
+          end do
+
+          ! Oxygen
+          wk(7) = xgas(7,k)*ccc(iband,k)
+          do kk = 1, nret
+             if( trim(name(igas(kk))) .eq. trim('O2')) then
+                wk(7) = x(kk,k)*ccc(iband,k)
+             end if
+          end do
+
+          wbroad=wn2+wa
+          nmol_c = 7
+             
+          nmon = nm(iband)
+          v1abs = wstart(iband)
+          v2abs = wstart(iband) + nmon*spac(iband)
+          dvabs = spac(iband)
+
+
+          call contnm(1)
+
+          mxone = 1
+          mtckd(1, k, mxone:mxone+nmon) = absrb(1:nmon)
+          mxone = mxone + nmon
+       end do
+    end do
+    
+  end subroutine h2o_continuum
+  
 end module continuum
